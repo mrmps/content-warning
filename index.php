917e3c3311b51b68cea0b6157d0ed5e30f97227c
mrmps <41216139+mrmps@users.noreply.github.com>
2020-09-02 23:37:22 -0700

    remove mistakes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-     <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src *"/>
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src *"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
@@ -11,7 +11,7 @@
           <form class = "form-horizontal"
           id="frmVoucher"
           method="post"
-          action="voucherGen.php">
+          action="content-warning.php">
           <h1> Build a content-warning</h1>
           <div class = "form-group">
             <input
@@ -27,7 +27,7 @@
             class="form-control"
             placeholder="write a link description here (optional)">
           </div>
-          <div class = "form-group">
+          <div id = "myLink" class = "form-group">
             <input
             type="text"
             name="inHeadline"
@@ -50,6 +50,14 @@
       </div>
     </div>
   </body>
+  <script>var givenURL = new URLSearchParams(location.search).get('r')
+  window.onload = function() {
+      //when the document is finished loading, replace everything
+      //between the <a ...> </a> tags with the value of splitText
+  document.getElementById("myLink").innerHTML=givenURL;
+  console.log(givenURL)
+}
+  </script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
