5fdf46a68a121e0202167dd6e260f04bf332c960
mrmps <41216139+mrmps@users.noreply.github.com>
2020-09-03 00:11:47 -0700

    link

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
             class="form-control"
             placeholder="write a link description here (optional)">
           </div>
-          <div id = "myLink" class = "form-group">
+          <div  class = "form-group">
             <input
             type="text"
             name="inHeadline"
@@ -50,14 +50,6 @@
       </div>
     </div>
   </body>
-  <script>var givenURL = new URLSearchParams(location.search).get('r')
-  window.onload = function() {
-      //when the document is finished loading, replace everything
-      //between the <a ...> </a> tags with the value of splitText
-  document.getElementById("myLink").innerHTML=givenURL;
-  console.log(givenURL)
-}
-  </script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
